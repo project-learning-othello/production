e016a191336fa0f626edb0facfb9c41450c049cb
RyoRyoRyooochan <72729465+RyoRyoRyooochan@users.noreply.github.com>
2021-05-11 14:37:38 +0900

    Add files via upload

---
+++ Player_Program/PlayerSample1.java
@@ -0,0 +1,13 @@
+
+public class PlayerSample1 {
+
+	private String myColor = ""; //先手後手情報(白黒)
+
+	// メソッド
+	public void setColor(String c){ // 先手後手情報の受付
+		myColor = c;
+	}
+	public String getColor(){ // 先手後手情報の取得
+		return myColor;
+	}
+}
