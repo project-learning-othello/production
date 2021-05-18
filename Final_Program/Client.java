bff3fe800f2cbc39b41385e22ae2c18e3c05c830
Nakanishi-Ryo <72484319+Nakanishi-Ryo@users.noreply.github.com>
2021-05-18 13:16:22 +0900

    Add files via upload

---
+++ Final_Program/Client.java
@@ -0,0 +1,168 @@
+//パッケージのインポート
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+
+public class Client extends JFrame implements MouseListener {
+	private JButton buttonArray[];//オセロ盤用のボタン配列
+	private JLabel numLabel1,numLabel2,numLabel_white,numLabel_black;// 駒数表示用
+	private JLabel nameLabel1,nameLabel2,nameLabel_white,nameLabel_black;// 手番表示用
+	private Container c; // コンテナ
+	private ImageIcon blackIcon, whiteIcon, boardIcon, putIcon; //アイコン
+	private PrintWriter out;//データ送信用オブジェクト
+	private Othello game; // Othelloオブジェクト
+	private Player player; // Playerオブジェクト
+	private String [] grids;
+	private int row; //オセロ盤の縦横マスの数
+	private String color; // 先手後手情報
+
+	// コンストラクタ
+	public Client(Othello game, Player player) {
+		this.game = game; // 引数のOthelloオブジェクトを渡す
+		row = game.getRow(); //getRowメソッドによりオセロ盤の縦横マスの数を取得
+		game.okPut("black");
+		grids = new String[row * row];
+		grids = game.getGrids(); //getGridメソッドにより局面情報を取得
+		this.player = player; // 引数のPlayerオブジェクトを渡す
+		color = player.getColor();
+
+		//ウィンドウ設定
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//ウィンドウを閉じる場合の処理
+		setTitle("ネットワーク対戦型オセロゲーム");//ウィンドウのタイトル
+		setSize(row * 45 + 25, row * 45 + 250);//ウィンドウのサイズを設定
+		c = getContentPane();//フレームのペインを取得
+
+		//アイコン設定(画像ファイルをアイコンとして使う)
+		whiteIcon = new ImageIcon("White.jpg");
+		blackIcon = new ImageIcon("Black.jpg");
+		boardIcon = new ImageIcon("GreenFrame.jpg");
+		putIcon = new ImageIcon("Put.jpg");
+
+		//描画
+		paint();
+	}
+
+	// メソッド
+	public void paint() { // 画面を描画する
+		c.setLayout(null);//
+		//オセロ盤の生成
+		buttonArray = new JButton[row * row];//ボタンの配列を作成
+		for(int i = 0 ; i < row * row ; i++){
+			if(grids[i].equals("black")){ buttonArray[i] = new JButton(blackIcon);}//盤面状態に応じたアイコンを設定
+			if(grids[i].equals("white")){ buttonArray[i] = new JButton(whiteIcon);}//盤面状態に応じたアイコンを設定
+			if(grids[i].equals("board")){ buttonArray[i] = new JButton(boardIcon);}//盤面状態に応じたアイコンを設定
+			if(grids[i].equals("put")){
+				buttonArray[i] = new JButton(putIcon); //盤面状態に応じたアイコンを設定
+				buttonArray[i].addMouseListener(this);//マウス操作を認識できるようにする
+			}
+			c.add(buttonArray[i]);//ボタンの配列をペインに貼り付け
+			// ボタンを配置する
+			int x = (i % row) * 45;
+			int y = (int) (i / row) * 45;
+			buttonArray[i].setBounds(x, y, 45, 45);//ボタンの大きさと位置を設定する．
+			buttonArray[i].setActionCommand(Integer.toString(i));//ボタンを識別するための名前(番号)を付加する
+		}
+
+		//駒数表示用ラベル
+		numLabel1 = new JLabel(whiteIcon);
+		numLabel1.setBounds(180, 370, 45, 45);
+		c.add(numLabel1);
+		numLabel_white = new JLabel(Integer.toString(game.get_num_white()));
+		numLabel_white.setBounds(230, 370, 45, 45); //境界を設定
+		numLabel_white.setFont(new Font("Arial",Font.PLAIN,24));
+		c.add(numLabel_white);
+
+		numLabel2 = new JLabel(blackIcon);
+		numLabel2.setBounds(270, 370, 45, 45);
+		c.add(numLabel2);
+		numLabel_black = new JLabel(Integer.toString(game.get_num_black()));
+		numLabel_black.setBounds(320, 370, 45, 45); //境界を設定
+		numLabel_black.setFont(new Font("Arial",Font.PLAIN,24));
+		c.add(numLabel_black);
+
+		//名前表示用ラベル
+		nameLabel_white = new JLabel(whiteIcon);
+		nameLabel_white.setBounds(20, 450, 40, 40);
+		nameLabel1 = new JLabel("あなた");
+		nameLabel1.setBounds(60, 450, 80, 40); //境界を設定
+		nameLabel1.setFont(new Font("Arial",Font.PLAIN,24));
+		nameLabel1.setBorder(new LineBorder(Color.YELLOW, 2, false));
+		c.add(nameLabel1);
+		c.add(nameLabel_white);
+
+		nameLabel_black = new JLabel(blackIcon);
+		nameLabel_black.setBounds(180, 450, 40, 40);
+		nameLabel2 = new JLabel("相手");
+		nameLabel2.setBounds(220, 450, 80, 40); //境界を設定
+		nameLabel2.setFont(new Font("Arial",Font.PLAIN,24));
+		nameLabel2.setBorder(new LineBorder(Color.BLACK, 2, false));
+		c.add(nameLabel2);
+		c.add(nameLabel_black);
+	}
+
+	public void connectServer(String ipAddress, int port){	// サーバに接続
+		Socket socket = null;
+		try {
+			socket = new Socket(ipAddress, port); //サーバ(ipAddress, port)に接続
+			out = new PrintWriter(socket.getOutputStream(), true); //データ送信用オブジェクトの用意
+		} catch (UnknownHostException e) {
+			System.err.println("ホストのIPアドレスが判定できません: " + e);
+			System.exit(-1);
+		} catch (IOException e) {
+			System.err.println("サーバ接続時にエラーが発生しました: " + e);
+			System.exit(-1);
+		}
+	}
+
+	public void sendMessage(String msg){	// サーバに操作情報を送信
+		out.println(msg);//送信データをバッファに書き出す
+		out.flush();//送信データを送る
+		System.out.println("サーバにメッセージ " + msg + " を送信しました"); //テスト標準出力
+	}
+
+	public void receiveMessage(String msg){	// メッセージの受信
+		System.out.println("サーバからメッセージ " + msg + " を受信しました"); //テスト用標準出力
+	}
+	public void updateDisp(){	// 画面を更新する
+		c.removeAll();
+		paint();
+	}
+	public void acceptOperation(String command){	// プレイヤの操作を受付
+	}
+
+  	//マウスクリック時の処理
+	public void mouseClicked(MouseEvent e) {
+		JButton theButton = (JButton)e.getComponent();//クリックしたオブジェクトを得る．キャストを忘れずに
+		String command = theButton.getActionCommand();//ボタンの名前を取り出す
+		System.out.println("マウスがクリックされました。押されたボタンは " + command + "です。");//テスト用に標準出力
+//		sendMessage(command); //テスト用にメッセージを送信
+		grids[Integer.parseInt(command)] = color;
+		updateDisp();
+	}
+	public void mouseEntered(MouseEvent e) {}//マウスがオブジェクトに入ったときの処理
+	public void mouseExited(MouseEvent e) {}//マウスがオブジェクトから出たときの処理
+	public void mousePressed(MouseEvent e) {}//マウスでオブジェクトを押したときの処理
+	public void mouseReleased(MouseEvent e) {}//マウスで押していたオブジェクトを離したときの処理
+
+	//テスト用のmain
+	public static void main(String args[]){
+		Othello game = new Othello();
+		Player player = new Player();
+		player.setColor("black");
+		Client oclient = new Client(game, player);
+		oclient.setVisible(true);
+//		oclient.connectServer("localhost", 10000);
+	}
+}
---
+++ Final_Program/Othello.java
@@ -0,0 +1,194 @@
+public class Othello {
+	private int row = 8;	// オセロ盤の縦横マス数
+	private String [][] grids = new String [row][row]; // 局面情報
+	private String turn; // 手番
+	private int num_turn; // 手数
+	private static int direction[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+	                                    //   UP,    DOWN,    LEFT,   RIGHT,  UPLEFT,  UPRIGHT, DOWNLEFT, DOWNRIGHT
+	private static int direction_num = 8;
+
+	// コンストラクタ
+	public Othello(){
+		int center = row / 2;
+
+		turn = "black"; // 黒が先手
+		num_turn = 0;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < row; j++) {
+				grids[j][i] = "board"; // 初めは石が置かれていない
+				grids[center - 1][center - 1] = "white";
+				grids[center - 1][center] = "black";
+				grids[center][center - 1] = "black";
+				grids[center][center] = "white";
+			}
+		}
+	}
+
+	// メソッド
+	public String checkWinner(){	// 勝敗を判断
+		if(get_num_black() == get_num_white()) {
+			return "draw";
+		}else if(get_num_black() > get_num_white()) {
+			return "black";
+		}else {
+			return "white";
+		}
+	}
+
+	public String getTurn(){ // 手番情報を取得
+		return turn;
+	}
+
+	public String [] getGrids(){ // 局面情報を取得
+		String[] one_dimentional_grids = new String[row * row];
+
+		for(int i = 0; i < row; i++){ // 2次元配列を1次元配列に変換
+			for(int j = 0; j < row; j++) {
+				one_dimentional_grids[j + i * row] = grids[j][i];
+			}
+		}
+
+		return one_dimentional_grids;
+	}
+
+	public void changeTurn(){ // 手番を変更
+		if(turn == "black") {
+			turn = "white";
+		}else {
+			turn = "black";
+		}
+	}
+
+	public boolean isGameover(){ // 対局終了を判断
+		if(okPut("black") == false) {
+			remove_put();
+			if(okPut("white") == false) {
+				remove_put();
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
+
+	public boolean okPut(String color) {
+		boolean board_flag[] = {false, false, false, false, false, false, false, false};
+		boolean put_flag = false;
+		String notColor;
+
+		if(color == "black") {
+			notColor = "white";
+		}else {
+			notColor = "black";
+		}
+
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < row; j++) {
+				if(grids[j][i] == notColor) {
+					for(int k = 0; k < direction_num; k++) {
+						if(j + direction[k][0] >= 0 && j + direction[k][0] <= row - 1 && i + direction[k][1] >= 0 && i + direction[k][1] <= row - 1) {
+							if(grids[j + direction[k][0]][i + direction[k][1]] == "board") {
+								board_flag[k] = true;
+							}
+						}
+
+						int x = i, y = j;
+
+						if(board_flag[k]) {
+							while(grids[y][x] == notColor) {
+								if(y - direction[k][0] >= 0 && y - direction[k][0] <= row - 1 && x - direction[k][1] >= 0 && x - direction[k][1] <= row - 1) {
+									y = y - direction[k][0];
+									x = x - direction[k][1];
+								}
+							}
+
+							if(grids[y][x] == color) {
+								grids[j + direction[k][0]][i + direction[k][1]] = "put";
+								put_flag = true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return put_flag;
+	}
+
+	public void putStone(int put_place, String color, boolean effect_on){ // (操作を)局面に反映
+		String notColor;
+
+		if(color == "black") {
+			notColor = "white";
+		}else {
+			notColor = "black";
+		}
+
+		for(int k = 0; k < direction_num; k++) {
+			int x = put_place % row, y = put_place / row;
+
+			if(y + direction[k][0] >= 0 && y + direction[k][0] <= row - 1 && x + direction[k][1] >= 0 && x + direction[k][1] <= row - 1) {
+				y = y + direction[k][0];
+				x = x + direction[k][1];
+			}
+
+			while(grids[y][x] == notColor) {
+				if(y + direction[k][0] >= 0 && y + direction[k][0] <= row - 1 && x + direction[k][1] >= 0 && x + direction[k][1] <= row - 1) {
+					y = y + direction[k][0];
+					x = x + direction[k][1];
+				}
+			}
+
+			if(grids[y][x] == color) {
+				while(x == put_place % row && y == put_place / row) {
+					y = y - direction[k][0];
+					x = x - direction[k][1];
+
+					grids[y][x] = color;
+				}
+			}
+
+			remove_put();
+		}
+	}
+
+	public void remove_put() {
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < row; j++) {
+				if(grids[j][i] == "put") {
+					grids[j][i] = "board";
+				}
+			}
+		}
+	}
+
+	public int getRow(){ //縦横のマス数を取得
+		return row;
+	}
+
+	public int get_num_white() { // 白の駒数を取得
+		int nw = 0;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < row; j++) {
+				if(grids[j][i] == "white") {
+					nw++;
+				}
+			}
+		}
+		return nw;
+	}
+
+	public int get_num_black() { // 黒の駒数を取得
+		int nb = 0;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < row; j++) {
+				if(grids[j][i] == "black") {
+					nb++;
+				}
+			}
+		}
+		return nb;
+	}
+}
