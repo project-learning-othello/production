e59c67ebdfa5e620343f08da9926edd233ac444a
RyoRyoRyooochan <72729465+RyoRyoRyooochan@users.noreply.github.com>
2021-05-22 17:12:50 +0900

    Add files via upload

--- OTHELLO2/src/Computer.java
+++ OTHELLO2/src/Computer.java
@@ -42,8 +42,8 @@ public class Computer {
 
 	//コピーメソッド
 	public void copy_grids(int a[][],int b[][]) {		//aにbを書き写す、a←b
-		for(int i=1;i<9;i++) {
-			for(int j=1;j<9;j++) {
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++) {
 				a[i][j]=b[i][j];
 			}
 		}
@@ -290,8 +290,7 @@ public class Computer {
 	public void search_hard() {
 		int count1=0,count2=0;
 		select1=64;select2=64;
-		reset_point(point1,-1000);
-		reset_point(point2,1000);
+		reset_point(point1,-10000);
 		for(int i=1;i<9;i++) {
 			for(int j=1;j<9;j++) {
 				if(grids_possible[i][j] == 3) {		//パスの場合を考える必要あり
@@ -300,6 +299,7 @@ public class Computer {
 					copy_grids(grids_temp1_possible,grids_temp1);
 					turnover_possible(grids_temp1_possible,grids_temp1,yourColor,myColor);
 					count2=0;
+					reset_point(point2,10000);
 					for(int k=1;k<9;k++) {
 						for(int l=1;l<9;l++) {
 							if(grids_temp1_possible[k][l] == 3) {		//パスの場合を考える必要あり
@@ -317,16 +317,16 @@ public class Computer {
 								point2[count2]-=count_possible(grids_temp2_possible);	//相手の打てるマス数
 
 								//評価値計算
-								point2[count2]+=count_x(grids_temp2,yourColor)*10;	//角なし時の相手のX数
-								point2[count2]-=count_x(grids_temp2,myColor)*10;	//角なし時の自分のX数
+								point2[count2]+=count_x(grids_temp2,yourColor,myColor)*10;	//角なし時の相手のX数
+								point2[count2]-=count_x(grids_temp2,myColor,yourColor)*10;	//角なし時の自分のX数
 
 								copy_grids(grids_keep,grids_temp2);
 								//評価値計算
-								point2[count2]+=count_keep(grids_keep,myColor)*10;		//自分の確定石数
+								point2[count2]+=count_keep(grids_keep,myColor)*100;		//自分の確定石数
 
 								copy_grids(grids_keep,grids_temp2);
 								//評価値計算
-								point2[count2]-=count_keep(grids_keep,yourColor)*10;	//相手の確定石数
+								point2[count2]-=count_keep(grids_keep,yourColor)*100;	//相手の確定石数
 
 								if(point2[select2]>point2[count2]) {
 									select2=count2;
@@ -351,16 +351,19 @@ public class Computer {
 	//ノーマルモード
 	public void search_normal() {
 		int count1,count2=0;
+		select1=64;
 		count1=count_possible(grids_possible);
-		Random r = new Random();
-		int n =r.nextInt(count1);
-		for(int i=1;i<9;i++) {
-			for(int j=1;j<9;j++) {
-				if(grids_possible[i][j] == 3) {
-					if(count2==n) {
-						select1=(i-1)*8+(j-1);
+		if(count1!=0) {
+			Random r = new Random();
+			int n =r.nextInt(count1);
+			for(int i=1;i<9;i++) {
+				for(int j=1;j<9;j++) {
+					if(grids_possible[i][j] == 3) {
+						if(count2==n) {
+							select1=(i-1)*8+(j-1);
+						}
+						count2++;
 					}
-					count2++;
 				}
 			}
 		}
@@ -374,7 +377,6 @@ public class Computer {
 		int count1=0,count2=0;
 		select1=64;select2=64;
 		reset_point(point1,10000);
-		reset_point(point2,-10000);
 		for(int i=1;i<9;i++) {
 			for(int j=1;j<9;j++) {
 				if(grids_possible[i][j] == 3) {		//パスの場合を考える必要あり
@@ -383,6 +385,7 @@ public class Computer {
 					copy_grids(grids_temp1_possible,grids_temp1);
 					turnover_possible(grids_temp1_possible,grids_temp1,yourColor,myColor);
 					count2=0;
+					reset_point(point2,-10000);
 					for(int k=1;k<9;k++) {
 						for(int l=1;l<9;l++) {
 							if(grids_temp1_possible[k][l] == 3) {		//パスの場合を考える必要あり
@@ -400,8 +403,8 @@ public class Computer {
 								point2[count2]-=count_possible(grids_temp2_possible);	//相手の打てるマス数
 
 								//評価値計算
-								point2[count2]+=count_x(grids_temp2,yourColor)*10;	//角なし時の相手のX数
-								point2[count2]-=count_x(grids_temp2,myColor)*10;	//角なし時の自分のX数
+								point2[count2]+=count_x(grids_temp2,yourColor,myColor)*10;	//角なし時の相手のX数
+								point2[count2]-=count_x(grids_temp2,myColor,yourColor)*10;	//角なし時の自分のX数
 
 								copy_grids(grids_keep,grids_temp2);
 								//評価値計算
@@ -488,27 +491,103 @@ public class Computer {
 		return flag;
 	}
 
-	//探索の部品(角がないのにXに自分の色がある数）
-	public int count_x(int a[][],int c1) {
+	//探索の部品(角がないのにXやCに自分の色がある数）
+	public int count_x(int a[][],int c1,int c2) {
 		int count=0;
+		int k=0;
 		if(a[1][1]==0) {
 			if(a[2][2]==c1) {
 				count++;
 			}
+			if(a[1][2]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[1][1+k] == c1);
+				if(a[1][1+k] == c2) {
+					count++;
+				}
+			}
+			if(a[2][1]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[1+k][1] == c1);
+				if(a[1+k][1] == c2) {
+					count++;
+				}
+			}
 		}
+
 		if(a[1][8]==0) {
 			if(a[2][7]==c1) {
 				count++;
 			}
+			if(a[1][7]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[1][8-k] == c1);
+				if(a[1][8-k] == c2) {
+					count++;
+				}
+			}
+			if(a[2][8]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[1+k][8] == c1);
+				if(a[1+k][8] == c2) {
+					count++;
+				}
+			}
 		}
+
 		if(a[8][1]==0) {
 			if(a[7][2]==c1) {
 				count++;
 			}
+			if(a[8][2]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[8][1+k] == c1);
+				if(a[8][1+k] == c2) {
+					count++;
+				}
+			}
+			if(a[7][1]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[8-k][1] == c1);
+				if(a[8-k][1] == c2) {
+					count++;
+				}
+			}
 		}
+
 		if(a[8][8]==0) {
 			if(a[7][7]==c1) {
 				count++;
+			}
+			if(a[8][7]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[8][8-k] == c1);
+				if(a[8][8-k] == c2) {
+					count++;
+				}
+			}
+			if(a[7][8]==c1) {
+				k=0;
+				do{
+					k++;
+				}while(a[8-k][8] == c1);
+				if(a[8-k][8] == c2) {
+					count++;
+				}
 			}
 		}
 
@@ -523,6 +602,21 @@ public class Computer {
 		copy_grids(grids_possible,grids);		//盤面をコピー
 		turnover_possible(grids_possible,grids,myColor,yourColor);//打てるマス記入
 		search();		//～～～～～これでselect1が最適の手になる～～～～～
+
+
+
+		//ここからテスト
+		System.out.println(point1[select1]);
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<8;j++) {
+				System.out.printf("%6d",point1[8*i+j]);
+			}
+			System.out.println("");
+		}
+		//ここまでテスト
+
+
+
 		if(select1!=64) {
 			turnover(grids,select1/8+1,select1%8+1,myColor,yourColor);		//ひっくり返す
 		}
