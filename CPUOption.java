6002939f132be7eea2b6e4a030e40b259d7be3b9
Noro-187 <72486946+Noro-187@users.noreply.github.com>
2021-05-23 19:22:33 +0900

    Add files via upload

--- CPUOption.java
+++ CPUOption.java
@@ -1,4 +1,4 @@
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,7 +14,7 @@ public class CPUOption extends JDialog implements ActionListener{
 
 	private Othello game; // Othelloオブジェクト
 	private Player player; // Playerオブジェクト
-	private Computer cpu; // Computerオブジェクト
+	private Computer cpu;
 
 	private JLabel nameLabel;
 
@@ -27,69 +27,17 @@ public class CPUOption extends JDialog implements ActionListener{
 	OMainFrame mainFrame;
 
 	ButtonGroup bg1 = new ButtonGroup();
+
 	ButtonGroup bg2 = new ButtonGroup();
 
-	JRadioButton r11 = new JRadioButton("先手",blackIcon);
-	JRadioButton r12 = new JRadioButton("後手",whiteIcon);
+	JRadioButton r11 = new JRadioButton("先手",true);
+	JRadioButton r12 = new JRadioButton("後手");
 
-	JRadioButton r21 = new JRadioButton("easy");
+	JRadioButton r21 = new JRadioButton("easy",true);
 	JRadioButton r22 = new JRadioButton("normal");
 	JRadioButton r23 = new JRadioButton("hard");
 
 	String mode;
-
-	public CPUOption(OMainFrame mainFrame, ModalityType mt){
-		super(mainFrame, mt);
-
-		game = new Othello();
-		player = new Player();
-
-		whiteIcon = new ImageIcon("White.jpg");
-		blackIcon = new ImageIcon("Black.jpg");
-
-		this.mainFrame = mainFrame;
-
-		this.setTitle("CPU設定");
-		this.setSize(8 * 45 + 25, 8 * 45 + 230);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setLayout(new GridLayout(6, 1));
-
-		this.nameLabel = new JLabel("CPU設定");
-		this.add(this.nameLabel);
-
-		this.nameLabel = new JLabel("先手後手");
-		this.add(this.nameLabel);
-
-		bg1.add(r11);
-		bg1.add(r12);
-		this.r11.setActionCommand("black");
-		this.r11.addActionListener(this);
-		this.r12.setActionCommand("white");
-		this.r12.addActionListener(this);
-		this.add(r11);
-		this.add(r12);
-
-		this.nameLabel = new JLabel("CPUの強さ");
-		this.add(this.nameLabel);
-
-		bg2.add(r21);
-		bg2.add(r22);
-		bg2.add(r23);
-		this.r21.setActionCommand("easy");
-		this.r21.addActionListener(this);
-		this.r22.setActionCommand("normal");
-		this.r22.addActionListener(this);
-		this.r23.setActionCommand("hard");
-		this.r23.addActionListener(this);
-		this.add(r21);
-		this.add(r22);
-		this.add(r23);
-
-		this.start = new JButton("対戦開始");
-		this.start.addActionListener(this);
-		this.add(this.start);
-
-	}
 
 	public void actionPerformed(ActionEvent e){
 		if(e.getSource() == this.r11){
@@ -109,8 +57,74 @@ public class CPUOption extends JDialog implements ActionListener{
 		}
 		if(e.getSource() == this.start){
 			VSCPU subWindow = new VSCPU(game,player,cpu, ModalityType.DOCUMENT_MODAL);
-			subWindow.setLocation(this.getLocation().x + (this.getWidth() - subWindow.getWidth()) / 2, this.getLocation().y + this.getHeight());
+			subWindow.setLocation(this.getLocation().x + this.getWidth() , this.getLocation().y);
 			subWindow.setVisible(true);
 		}
 	}
+
+	public CPUOption(OMainFrame mainFrame, ModalityType mt){
+		super(mainFrame, mt);
+
+		whiteIcon = new ImageIcon("White.jpg");
+		blackIcon = new ImageIcon("Black.jpg");
+
+		this.mainFrame = mainFrame;
+		game = new Othello();
+		player = new Player();
+
+		this.setTitle("CPU設定");
+		this.setSize(8 * 45 + 25, 8 * 45 + 230);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLayout(null);
+
+		this.nameLabel = new JLabel("CPU設定");
+		nameLabel.setFont(new Font( "ＭＳ ゴシック" , Font.BOLD, 30));
+		nameLabel.setBounds(120, 20, 150, 50);
+		this.add(this.nameLabel);
+
+		this.nameLabel = new JLabel("先手後手");
+		nameLabel.setFont(new Font( "ＭＳ ゴシック" , Font.BOLD, 30));
+		nameLabel.setBounds(115, 100, 300, 50);
+		this.add(this.nameLabel);
+
+		bg1.add(r11);
+		bg1.add(r12);
+		this.r11.setActionCommand("");
+		this.r11.addActionListener(this);
+		this.r12.setActionCommand("");
+		this.r12.addActionListener(this);
+
+		r11.setBounds(90, 180, 50, 50);
+		r12.setBounds(220, 180, 50, 50);
+		this.add(r11);
+		this.add(r12);
+
+		this.nameLabel = new JLabel("CPUの強さ");
+		nameLabel.setFont(new Font( "ＭＳ ゴシック" , Font.BOLD, 30));
+		nameLabel.setBounds(110, 260, 300, 50);
+		this.add(this.nameLabel);
+
+		bg2.add(r21);
+		bg2.add(r22);
+		bg2.add(r23);
+		this.r21.setActionCommand("");
+		this.r21.addActionListener(this);
+		this.r22.setActionCommand("");
+		this.r22.addActionListener(this);
+		this.r23.setActionCommand("");
+		this.r23.addActionListener(this);
+		r21.setBounds(60, 340, 70, 50);
+		r22.setBounds(140, 340, 70, 50);
+		r23.setBounds(250, 340, 70, 50);
+		this.add(r21);
+		this.add(r22);
+		this.add(r23);
+
+		this.start = new JButton("対戦開始");
+		this.start.addActionListener(this);
+		start.setBounds(130, 430,90,50);
+		this.add(this.start);
+
+	}
+
 }
